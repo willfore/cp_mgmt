31b37d7398430b6095aeb6bdd590abe98c1ca9d6
Billy Forester <william.forester@gmail.com>
2018-11-09 10:36:30 -0600

    bump version for new PR

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CpMgmt.MixProject do
   def project do
     [
       app: :cp_mgmt,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
