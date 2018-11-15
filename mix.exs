9234c489e54a71a60cd93dc176f0f2a481366016
Billy Forester <william.forester@gmail.com>
2018-11-15 12:54:00 -0600

    updated version for RC0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CpMgmt.MixProject do
   def project do
     [
       app: :cp_mgmt,
-      version: "0.0.7",
+      version: "1.0.0-rc0",
       elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
