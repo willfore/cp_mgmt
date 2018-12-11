7652c5102e0bf154293f97364609a4266bd70bd2
Billy Forester <william.forester@gmail.com>
2018-12-11 09:58:02 -0600

    add ability to use with MDS

--- config/prod.exs
+++ config/prod.exs
@@ -1,12 +1,9 @@
 use Mix.Config
-# below stores the parameters to your first login for re-use with other commands
-config :cp_mgmt, :api,
-  sid: "",
-  uid: "",
-  timeout: 600
-
-# set params below according to the location of your management server
+# set params below according to the IP and PORT of your management server (Must be HTTPS)
 config :cp_mgmt, mgmt_server_url: "https://192.168.0.8:443"
+config :cp_mgmt, mgmt_user: "admin"
+config :cp_mgmt, mgmt_pass: "vpn123"
+config :cp_mgmt, mgmt_domain: nil
 
 # Set this paramater for what Adapter Tesla will use (hackney is recommended)
 # If your management server has an authenticate certification uncomment the line below and comment out
